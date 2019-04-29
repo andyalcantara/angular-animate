39450c79f9ffc1c9ae67e6a61bfc39d0e77fef4b
Andy Alcantara <andyalcantara745@yahoo.com>
2019-04-29 09:16:30 -0400

    Add first animation with directive

--- src/app/directives/rotate-directive.directive.ts
+++ src/app/directives/rotate-directive.directive.ts
@@ -1,10 +1,38 @@
-import { Directive } from '@angular/core';
+import {Directive, Injectable, ElementRef, Renderer2, HostListener} from '@angular/core';
+import { AnimationBuilder, style, animate } from '@angular/animations';
 
+@Injectable()
 @Directive({
   selector: '[appRotateDirective]'
 })
 export class RotateDirectiveDirective {
 
-  constructor() { }
+  deg = 0;
+
+  constructor(
+    private animationBuilder: AnimationBuilder,
+    private el: ElementRef,
+    private renderer: Renderer2
+  ) { }
+
+  @HostListener('click') onMouseClick() {
+    if (this.deg === 0) {
+      this.deg = 180;
+    } else if (this.deg > 0) {
+      this.deg = this.deg + 180;
+    }
+    const directiveAnimation = this.animationBuilder.build([
+      style({
+        backgroundColor: 'white',
+      }),
+      animate(500, style({
+        backgroundColor: 'white',
+        transform: `rotateY(${this.deg}deg)`
+      }))
+    ]);
+
+    const player = directiveAnimation.create(this.el.nativeElement);
+    player.play();
+  }
 
 }
