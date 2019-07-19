04401eb98bc0b258084526471d885f455df68d0c
Andy Alcantara <andyalcantara745@yahoo.com>
2019-07-19 15:11:53 -0400

    Add flash animation directive

--- src/app/directives/flash.directive.ts
+++ src/app/directives/flash.directive.ts
@@ -1,10 +1,36 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, Injectable } from '@angular/core';
+import { AnimationBuilder, style, animate } from '@angular/animations';
 
+@Injectable()
 @Directive({
   selector: '[appFlash]'
 })
 export class FlashDirective {
 
-  constructor() { }
+  constructor(
+    private animationBuilder: AnimationBuilder,
+    private element: ElementRef
+  ) {
+    const directiveAnimation = animationBuilder.build([
+      animate('300ms ease-in', style({
+        opacity: 1
+      })),
+      animate('200ms ease-out', style({
+        opacity: 0
+      })),
+      animate('300ms ease-in', style({
+        opacity: 1
+      })),
+      animate('200ms ease-out', style({
+        opacity: 0
+      })),
+      animate('300ms ease-in', style({
+        opacity: 1
+      }))
+    ]);
+
+    const player = directiveAnimation.create(element.nativeElement);
+    player.play();
+  }
 
 }
