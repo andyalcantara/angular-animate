ab264f48db3875fd3a8760d8fa84748b64a32ce6
Andy Alcantara <andyalcantara745@yahoo.com>
2019-07-20 23:25:10 -0400

    Add first logic attempt to rubber

--- src/app/directives/rubber.directive.ts
+++ src/app/directives/rubber.directive.ts
@@ -1,10 +1,36 @@
-import { Directive } from '@angular/core';
+import {Directive, Injectable, ElementRef, Input} from '@angular/core';
+import { AnimationBuilder, style, animate } from '@angular/animations';
 
+@Injectable()
 @Directive({
   selector: '[appRubber]'
 })
 export class RubberDirective {
 
-  constructor() { }
+  constructor(
+    private animationBuilder: AnimationBuilder,
+    private element: ElementRef
+  ) {
+    const directiveAnimation = animationBuilder.build([
+      animate('100ms ease-in', style({
+        transform: 'scaleX(2.0)'
+      })),
+      animate('50ms', style({
+        transform: 'scaleX(0.96)'
+      })),
+      animate('100ms ease-in', style({
+        transform: 'scaleX(1.0)'
+      })),
+      animate('50ms', style({
+        transform: 'scaleX(1.50)'
+      })),
+      animate('200ms ease-in', style({
+        transform: 'scaleX(1.0)'
+      })),
+    ]);
+
+    const player = directiveAnimation.create(this.element.nativeElement);
+    player.play();
+  }
 
 }
