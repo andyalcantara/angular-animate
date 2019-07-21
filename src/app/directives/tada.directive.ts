bf6089d9c9af9908dd26bd9bd88736f14194449b
Andy Alcantara <andyalcantara745@yahoo.com>
2019-07-21 00:07:10 -0400

    Implement animation logic to tada

--- src/app/directives/tada.directive.ts
+++ src/app/directives/tada.directive.ts
@@ -1,10 +1,47 @@
-import { Directive } from '@angular/core';
+import {Directive, ElementRef, Injectable} from '@angular/core';
+import {AnimationBuilder, animate, style } from '@angular/animations';
 
+@Injectable()
 @Directive({
   selector: '[appTada]'
 })
 export class TadaDirective {
 
-  constructor() { }
+  constructor(
+    private animationBuilder: AnimationBuilder,
+    private element: ElementRef
+  ) {
+    const directiveAnimation = animationBuilder.build([
+      animate('200ms ease-in', style({
+        transform: 'scale(0.80) rotate(-15deg)'
+      })),
+      animate('100ms ease-in', style({
+        transform: 'scale(1.0) rotate(-5deg)'
+      })),
+      animate('100ms ease-in', style({
+        transform: 'rotate(5deg)'
+      })),
+      animate('100ms ease-in', style({
+        transform: 'rotate(-5deg)'
+      })),
+      animate('100ms ease-in', style({
+        transform: 'rotate(5deg)'
+      })),
+      animate('100ms ease-in', style({
+        transform: 'rotate(-5deg)'
+      })),
+      animate('100ms ease-in', style({
+        transform: 'rotate(5deg)'
+      })),
+      animate('100ms ease-in', style({
+        transform: 'rotate(-5deg)'
+      })),
+      animate('100ms ease-in', style({
+        transform: 'rotate(0deg)'
+      }))
+    ]);
+    const player = directiveAnimation.create(element.nativeElement);
+    player.play();
+  }
 
 }
