590a328d485ea8e052aca274359bc308c7f6f40f
Andy Alcantara <andyalcantara745@yahoo.com>
2019-07-20 23:51:09 -0400

    Implement animation steps to swing animation

--- src/app/directives/swing.directive.ts
+++ src/app/directives/swing.directive.ts
@@ -1,10 +1,35 @@
-import { Directive } from '@angular/core';
+import {Directive, Injectable, ElementRef, Input} from '@angular/core';
+import { AnimationBuilder, style, animate } from '@angular/animations';
 
+@Injectable()
 @Directive({
   selector: '[appSwing]'
 })
 export class SwingDirective {
 
-  constructor() { }
+  constructor(
+    private animationBuilder: AnimationBuilder,
+    private element: ElementRef
+  ) {
+    const directiveAnimation = animationBuilder.build([
+      animate('200ms ease-in', style({
+        transform: 'rotate(25deg)'
+      })),
+      animate('200ms ease-in', style({
+        transform: 'rotate(-25deg)'
+      })),
+      animate('200ms ease-in', style({
+        transform: 'rotate(5deg)'
+      })),
+      animate('200ms ease-in', style({
+        transform: 'rotate(-5deg)'
+      })),
+      animate('200ms ease-in', style({
+        transform: 'rotate(0deg)'
+      })),
+    ]);
+    const player = directiveAnimation.create(element.nativeElement);
+    player.play();
+  }
 
 }
