6b07be0d53e796e713ed48645d019ef838ee181d
Andy Alcantara <andyalcantara745@yahoo.com>
2019-07-20 22:23:24 -0400

    Add logic for animation  without dinaymic timing

--- src/app/directive/pulse.directive.ts
+++ src/app/directive/pulse.directive.ts
@@ -1,10 +1,28 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, Injectable } from '@angular/core';
+import { AnimationBuilder, style, animate } from '@angular/animations';
 
+@Injectable()
 @Directive({
   selector: '[appPulse]'
 })
 export class PulseDirective {
 
-  constructor() { }
+  constructor(
+    private animationBuilder: AnimationBuilder,
+    private element: ElementRef
+  ) {
+    const directiveAnimation = animationBuilder.build([
+      animate('100ms ease-in', style({
+        transform: 'scale(1.2)'
+      })),
+
+      animate('100ms ease-in', style({
+        transform: 'scale(1)'
+      })),
+    ]);
+
+    const player = directiveAnimation.create(this.element.nativeElement);
+    player.play();
+  }
 
 }
