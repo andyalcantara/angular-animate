a2dfec7011313aeb983685d23b243bf85b1494c1
Andy Alcantara <andyalcantara745@yahoo.com>
2019-07-24 17:23:40 -0400

    Add logic for wobble animation

--- src/app/directives/wobble.directive.ts
+++ src/app/directives/wobble.directive.ts
@@ -1,10 +1,41 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, Input } from '@angular/core';
+import { AnimationBuilder, style, animate } from '@angular/animations';
 
 @Directive({
   selector: '[appWobble]'
 })
 export class WobbleDirective {
 
-  constructor() { }
+  constructor(
+    private animationBuilder: AnimationBuilder,
+    private element: ElementRef
+  ) {
+    const directiveAnimation = animationBuilder.build([
+      animate('200ms ease-in', style({
+        transform: 'rotate(-10deg) translateX(-100px)'
+      })),
+      animate('100ms ease-in', style({
+        transform: 'rotate(10deg) translateX(100px)'
+      })),
+      animate('100ms ease-in', style({
+        transform: 'rotate(-10deg) translateX(-60px)'
+      })),
+      animate('100ms ease-in', style({
+        transform: 'rotate(10deg) translateX(60px)'
+      })),
+      animate('100ms ease-in', style({
+        transform: 'rotate(-10deg) translateX(-20px)'
+      })),
+      animate('100ms ease-in', style({
+        transform: 'rotate(10deg) translateX(20px)'
+      })),
+      animate('100ms ease-in', style({
+        transform: 'rotate(0deg) translateX(0)'
+      })),
+    ]);
+
+    const player = directiveAnimation.create(this.element.nativeElement);
+    player.play();
+  }
 
 }
