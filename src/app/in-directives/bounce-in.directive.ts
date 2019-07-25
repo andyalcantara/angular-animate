c86fe4e79b0cc08498a92e654b4d2b5d236b98e1
Andy Alcantara <andyalcantara745@yahoo.com>
2019-07-25 14:03:04 -0400

    Add bounce in animation

--- src/app/in-directives/bounce-in.directive.ts
+++ src/app/in-directives/bounce-in.directive.ts
@@ -1,10 +1,37 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, Input } from '@angular/core';
+import {AnimationBuilder, animate, style } from '@angular/animations';
 
 @Directive({
   selector: '[appBounceIn]'
 })
 export class BounceInDirective {
 
-  constructor() { }
+  constructor(
+    private animationBuilder: AnimationBuilder,
+    private element: ElementRef
+  ) {
+
+    const directiveAnimation = animationBuilder.build([
+      animate('0ms ease-in', style({
+        opacity: 0
+      })),
+      animate('200ms ease-in', style({
+        transform: 'scale(1.5)',
+        opacity: 1,
+      })),
+      animate('200ms ease-in', style({
+        transform: 'scale(1.0)'
+      })),
+      animate('200ms ease-in', style({
+        transform: 'scale(1.3)'
+      })),
+      animate('200ms ease-in', style({
+        transform: 'scale(1.0)'
+      }))
+    ]);
+
+    const player = directiveAnimation.create(this.element.nativeElement);
+    player.play();
+  }
 
 }
