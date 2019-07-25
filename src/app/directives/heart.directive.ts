88d9ff54910bee68e180e85da60417e4216ff4e7
Andy Alcantara <andyalcantara745@yahoo.com>
2019-07-25 11:32:50 -0400

    Implement logic for heart bitting animation

--- src/app/directives/heart.directive.ts
+++ src/app/directives/heart.directive.ts
@@ -1,10 +1,31 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, Input } from '@angular/core';
+import {AnimationBuilder, animate, style } from '@angular/animations';
 
 @Directive({
   selector: '[appHeart]'
 })
 export class HeartDirective {
 
-  constructor() { }
+  constructor(
+    private animationBuilder: AnimationBuilder,
+    private element: ElementRef
+  ) {
+    const directiveAnimation = animationBuilder.build([
+      animate('200ms ease-out', style({
+        transform: 'scale(1.5)'
+      })),
+      animate('100ms ease-in', style({
+        transform: 'scale(1.0)'
+      })),
+      animate('200ms ease-out', style({
+        transform: 'scale(1.5)'
+      })),
+      animate('100ms ease-in', style({
+        transform: 'scale(1.0)'
+      }))
+    ]);
+    const player = directiveAnimation.create(this.element.nativeElement);
+    player.play();
+  }
 
 }
