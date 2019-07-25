a760b1c8450622b875566745e35e826eea08a458
Andy Alcantara <andyalcantara745@yahoo.com>
2019-07-25 09:36:02 -0400

    Setting directive constructor function

--- src/app/directives/jello.directive.ts
+++ src/app/directives/jello.directive.ts
@@ -1,10 +1,22 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, Input } from '@angular/core';
+import { AnimationBuilder, style, animate } from '@angular/animations';
 
 @Directive({
   selector: '[appJello]'
 })
 export class JelloDirective {
 
-  constructor() { }
+  constructor(
+    private animationBuilder: AnimationBuilder,
+    private element: ElementRef
+  ) {
+    const directiveAnimation = animationBuilder.build([
+      animate('200ms ease-in', style({
+        transform: 'rotate(-30deg) scaleX(1.5)'
+      }))
+    ]);
+    const player = directiveAnimation.create(this.element.nativeElement);
+    player.play();
+  }
 
 }
