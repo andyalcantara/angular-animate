44e8edc107ecd45b8d069c162eb4e6c12c6604ef
Andy Alcantara <andyalcantara745@yahoo.com>
2019-07-26 15:45:07 -0400

    Implement animation logic for bounce out left

--- src/app/out-directives/bounce-out-left.directive.ts
+++ src/app/out-directives/bounce-out-left.directive.ts
@@ -1,10 +1,29 @@
-import { Directive } from '@angular/core';
+import {Directive, ElementRef} from '@angular/core';
+import {animate, AnimationBuilder, style} from '@angular/animations';
 
 @Directive({
   selector: '[appBounceOutLeft]'
 })
 export class BounceOutLeftDirective {
 
-  constructor() { }
+  constructor(
+    private animationBuilder: AnimationBuilder,
+    private element: ElementRef
+  ) {
+    const directiveAnimation = animationBuilder.build([
+      animate('200ms ease-in-out', style({
+        transform: 'translateX(-35px)'
+      })),
+      animate('200ms ease-in-out', style({
+        transform: 'translateX(0)'
+      })),
+      animate('180ms ease-in-out', style({
+        transform: 'translateX(-400px)',
+        opacity: 0
+      }))
+    ]);
+    const player = directiveAnimation.create(this.element.nativeElement);
+    player.play();
+  }
 
 }
