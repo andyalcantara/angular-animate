71d4f261f7124457d5dce7791c997c6611e53ee8
Andy Alcantara <andyalcantara745@yahoo.com>
2019-07-26 15:03:58 -0400

    Implement logic for bounce out down direcitve

--- src/app/out-directives/bounce-out-down.directive.ts
+++ src/app/out-directives/bounce-out-down.directive.ts
@@ -1,10 +1,29 @@
-import { Directive } from '@angular/core';
+import {Directive, ElementRef} from '@angular/core';
+import {animate, AnimationBuilder, style} from '@angular/animations';
 
 @Directive({
   selector: '[appBounceOutDown]'
 })
 export class BounceOutDownDirective {
 
-  constructor() { }
+  constructor(
+    private animationBuilder: AnimationBuilder,
+    private element: ElementRef
+  ) {
+    const directiveAnimation = animationBuilder.build([
+      animate('200ms ease-in-out', style({
+        transform: 'translateY(35px)'
+      })),
+      animate('200ms ease-in-out', style({
+        transform: 'translateY(0)'
+      })),
+      animate('180ms ease-in-out', style({
+        transform: 'translateY(400px)',
+        opacity: 0
+      }))
+    ]);
+    const player = directiveAnimation.create(this.element.nativeElement);
+    player.play();
+  }
 
 }
