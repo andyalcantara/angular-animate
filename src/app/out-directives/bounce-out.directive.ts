84bb6018be338d9b6d2e531fc7001875a51a8451
Andy Alcantara <andyalcantara745@yahoo.com>
2019-07-26 14:58:34 -0400

    Implement animation logic bounce out

--- src/app/out-directives/bounce-out.directive.ts
+++ src/app/out-directives/bounce-out.directive.ts
@@ -1,10 +1,32 @@
-import { Directive } from '@angular/core';
+import {Directive, ElementRef, Input, OnInit} from '@angular/core';
+import {animate, AnimationBuilder, style} from '@angular/animations';
 
 @Directive({
   selector: '[appBounceOut]'
 })
 export class BounceOutDirective {
 
-  constructor() { }
+  @Input() delay: number;
+  @Input() timing = '300ms';
+
+  constructor(
+    private animationBuilder: AnimationBuilder,
+    private element: ElementRef
+  ) {
+    const directiveAnimation = animationBuilder.build([
+       animate('200ms ease-in-out', style({
+         transform: 'scale(0.7)'
+       })),
+       animate('200ms ease-in-out', style({
+         transform: 'scale(1)'
+       })),
+       animate('200ms ease-in-out', style({
+         transform: 'scale(0)',
+         opacity: 0
+       }))
+    ]);
+    const player = directiveAnimation.create(this.element.nativeElement);
+    player.play();
+  }
 
 }
