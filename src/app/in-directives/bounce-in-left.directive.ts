e1c9a6aa8ff1fc0ec9dbc58cfd606f2c88017ff5
Andy Alcantara <andyalcantara745@yahoo.com>
2019-07-26 14:32:23 -0400

    Add bounce in left logic to directive

--- src/app/in-directives/bounce-in-left.directive.ts
+++ src/app/in-directives/bounce-in-left.directive.ts
@@ -1,10 +1,47 @@
-import { Directive } from '@angular/core';
+import {Directive, ElementRef, Input, OnInit} from '@angular/core';
+import {animate, AnimationBuilder, style} from '@angular/animations';
 
 @Directive({
   selector: '[appBounceInLeft]'
 })
-export class BounceInLeftDirective {
+export class BounceInLeftDirective implements OnInit {
 
-  constructor() { }
+  @Input() delay: number;
+  @Input() timing = '300ms';
 
+  constructor(
+    private animationBuilder: AnimationBuilder,
+    private element: ElementRef
+  ) { }
+
+  startAnimation() {
+    setTimeout(() => {
+      const directiveAnimation = this.animationBuilder.build([
+        style({
+          opacity: 0
+        }),
+        animate(`${this.timing} ease-in`, style({
+          opacity: 0,
+          transform: 'translateX(-500px)'
+        })),
+        animate(`300ms ease-in-out`, style({
+          opacity: 1,
+          transform: 'translateX(0)'
+        })),
+        animate(`100ms ease-in-out`, style({
+          transform: 'translateX(-20px)'
+        })),
+        animate(`200ms ease-in-out`, style({
+          transform: 'translateX(0)'
+        }))
+      ]);
+
+      const player = directiveAnimation.create(this.element.nativeElement);
+      player.play();
+    }, this.delay);
+  }
+
+  ngOnInit(): void {
+    this.startAnimation();
+  }
 }
