00d3ec7d8fec9472e89eedac5ac8659a30f21fc8
Andy Alcantara <andyalcantara745@yahoo.com>
2019-07-29 09:47:34 -0400

    Implement logic for animation

--- src/app/out-directives/bounce-out-right.directive.ts
+++ src/app/out-directives/bounce-out-right.directive.ts
@@ -1,10 +1,29 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, Input } from '@angular/core';
+import {AnimationBuilder, animate, style} from '@angular/animations';
 
 @Directive({
   selector: '[appBounceOutRight]'
 })
 export class BounceOutRightDirective {
 
-  constructor() { }
+  constructor(
+    private animationBuilder: AnimationBuilder,
+    private element: ElementRef
+  ) {
+    const directiveAnimation = animationBuilder.build([
+      animate('200ms ease-in-out', style({
+        transform: 'translateX(35px)'
+      })),
+      animate('200ms ease-in-out', style({
+        transform: 'translateX(0)'
+      })),
+      animate('180ms ease-in-out', style({
+        transform: 'translateX(400px)',
+        opacity: 0
+      }))
+    ]);
+    const player = directiveAnimation.create(this.element.nativeElement);
+    player.play();
+  }
 
 }
