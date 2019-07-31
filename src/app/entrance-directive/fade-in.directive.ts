9014d263064a293ac7375f7e6984dd9926a7bc01
Andy Alcantara <andyalcantara745@yahoo.com>
2019-07-31 16:53:17 -0400

    Implement logic for animation

--- src/app/entrance-directive/fade-in.directive.ts
+++ src/app/entrance-directive/fade-in.directive.ts
@@ -1,10 +1,27 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, Input } from '@angular/core';
+import { AnimationBuilder, animate, style } from '@angular/animations';
 
 @Directive({
   selector: '[appFadeIn]'
 })
 export class FadeInDirective {
 
-  constructor() { }
+  @Input() delay: string;
+
+  constructor(
+    private animationBuilder: AnimationBuilder,
+    private element: ElementRef
+  ) {
+    const directiveAnimation = animationBuilder.build([
+      animate('0ms ease-in-out', style({
+        opacity: 0
+      })),
+      animate('600ms ease-in-out', style({
+        opacity: 1
+      })),
+    ]);
+    const player = directiveAnimation.create(this.element.nativeElement);
+    player.play();
+  }
 
 }
