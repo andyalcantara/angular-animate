8c6ec793f77423f3ca604b874c88f0dec5916104
Andy Alcantara <andyalcantara745@yahoo.com>
2019-07-31 16:57:59 -0400

    Implement fade in down directive

--- src/app/entrance-directive/fade-in-down.directive.ts
+++ src/app/entrance-directive/fade-in-down.directive.ts
@@ -1,10 +1,27 @@
-import { Directive } from '@angular/core';
+import {Directive, ElementRef} from '@angular/core';
+import {animate, AnimationBuilder, style} from '@angular/animations';
 
 @Directive({
   selector: '[appFadeInDown]'
 })
 export class FadeInDownDirective {
 
-  constructor() { }
+  constructor(
+    private animationBuilder: AnimationBuilder,
+    private element: ElementRef
+  ) {
+    const directiveAnimation = animationBuilder.build([
+      animate('0ms ease-in-out', style({
+        opacity: 0,
+        transform: 'translateY(-300px)'
+      })),
+      animate('600ms ease-in-out', style({
+        opacity: 1,
+        transform: 'translateY(0)'
+      })),
+    ]);
+    const player = directiveAnimation.create(this.element.nativeElement);
+    player.play();
+  }
 
 }
