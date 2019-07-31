49965e289b259da152a3822c37df37ef58e29ba7
Andy Alcantara <andyalcantara745@yahoo.com>
2019-07-31 17:03:04 -0400

    Implement logic for fade in down big directive

--- src/app/entrance-directive/fade-in-down-big.directive.ts
+++ src/app/entrance-directive/fade-in-down-big.directive.ts
@@ -1,10 +1,27 @@
-import { Directive } from '@angular/core';
+import {Directive, ElementRef} from '@angular/core';
+import {animate, AnimationBuilder, style} from '@angular/animations';
 
 @Directive({
   selector: '[appFadeInDownBig]'
 })
 export class FadeInDownBigDirective {
 
-  constructor() { }
+  constructor(
+    private animationBuilder: AnimationBuilder,
+    private element: ElementRef
+  ) {
+    const directiveAnimation = animationBuilder.build([
+      animate('0ms ease-in-out', style({
+        opacity: 0,
+        transform: 'translateY(-500px)'
+      })),
+      animate('600ms ease-in-out', style({
+        opacity: 1,
+        transform: 'translateY(0)'
+      })),
+    ]);
+    const player = directiveAnimation.create(this.element.nativeElement);
+    player.play();
+  }
 
 }
