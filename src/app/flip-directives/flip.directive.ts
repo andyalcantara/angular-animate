aafc7b59b28b26fbde6b5be11481553e00346864
Andy Alcantara <andyalcantara745@yahoo.com>
2019-08-01 10:25:40 -0400

    Implement flip directive animation logic

--- src/app/flip-directives/flip.directive.ts
+++ src/app/flip-directives/flip.directive.ts
@@ -1,10 +1,25 @@
-import { Directive } from '@angular/core';
+import { Directive, Input, ElementRef } from '@angular/core';
+import { AnimationBuilder, style, animate } from '@angular/animations';
 
 @Directive({
   selector: '[appFlip]'
 })
 export class FlipDirective {
 
-  constructor() { }
+  constructor(
+    private animationBuilder: AnimationBuilder,
+    private element: ElementRef
+  ) {
+    const directiveAnimation = animationBuilder.build([
+      animate('500ms ease-in-out', style({
+        transform: 'scale(1.5) rotateY(180deg)'
+      })),
+      animate('500ms ease-in-out', style({
+        transform: 'scale(1) rotateY(360deg)'
+      }))
+    ]);
+    const player = directiveAnimation.create(this.element.nativeElement);
+    player.play();
+  }
 
 }
