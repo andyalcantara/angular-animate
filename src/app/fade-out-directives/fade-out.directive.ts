e6b0738c280df7e63566695267a13ae18112c4d3
Andy Alcantara <andyalcantara745@yahoo.com>
2019-08-01 09:36:01 -0400

    Implement logic fade out directive

--- src/app/fade-out-directives/fade-out.directive.ts
+++ src/app/fade-out-directives/fade-out.directive.ts
@@ -1,10 +1,25 @@
-import { Directive } from '@angular/core';
+import {Directive, ElementRef} from '@angular/core';
+import {animate, AnimationBuilder, style} from '@angular/animations';
 
 @Directive({
   selector: '[appFadeOut]'
 })
 export class FadeOutDirective {
 
-  constructor() { }
+  constructor(
+    private animationBuilder: AnimationBuilder,
+    private element: ElementRef
+  ) {
+    const directiveAnimation = animationBuilder.build([
+      animate('0ms ease-in-out', style({
+        opacity: 1
+      })),
+      animate('800ms ease-in-out', style({
+        opacity: 0
+      })),
+    ]);
+    const player = directiveAnimation.create(this.element.nativeElement);
+    player.play();
+  }
 
 }
