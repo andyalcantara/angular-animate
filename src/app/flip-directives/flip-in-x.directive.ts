cecde17786f2181a480be8171b008bbca348d2d7
Andy Alcantara <andyalcantara745@yahoo.com>
2019-08-02 10:38:53 -0400

    Add flip in x logic

--- src/app/flip-directives/flip-in-x.directive.ts
+++ src/app/flip-directives/flip-in-x.directive.ts
@@ -1,10 +1,27 @@
-import { Directive } from '@angular/core';
+import {Directive, ElementRef} from '@angular/core';
+import {animate, AnimationBuilder, style} from '@angular/animations';
 
 @Directive({
   selector: '[appFlipInX]'
 })
 export class FlipInXDirective {
 
-  constructor() { }
+  constructor(
+    private animationBuilder: AnimationBuilder,
+    private element: ElementRef
+  ) {
+    const directiveAnimation = animationBuilder.build([
+      animate('0ms ease-in-out', style({
+        opacity: 0.7,
+        transform: 'scale(1.5) rotateX(90deg)'
+      })),
+      animate('500ms ease-in-out', style({
+        opacity: 1,
+        transform: 'scale(1) rotateX(0deg)'
+      }))
+    ]);
+    const player = directiveAnimation.create(this.element.nativeElement);
+    player.play();
+  }
 
 }
