e32c127afac3ce8592215192618e3f4c1f17316c
Andy Alcantara <andyalcantara745@yahoo.com>
2019-08-06 22:45:10 -0400

    Add rotate in animation logic

--- src/app/rotate-entrance/rotate-in.directive.ts
+++ src/app/rotate-entrance/rotate-in.directive.ts
@@ -1,10 +1,29 @@
-import { Directive } from '@angular/core';
+import { Directive, Input, OnInit, ElementRef } from '@angular/core';
+import { AnimationBuilder, animate, style, keyframes } from '@angular/animations';
 
 @Directive({
   selector: '[animRotateIn]'
 })
-export class RotateInDirective {
+export class RotateInDirective implements OnInit {
 
-  constructor() { }
+  constructor(
+    private animationBuilder: AnimationBuilder,
+    private element: ElementRef
+  ) { }
+
+  ngOnInit(): void {
+    const directiveAnimation = this.animationBuilder.build([
+      animate('0ms ease-in-out', style({
+        opacity: 0,
+        transform: 'rotate(90deg)'
+      })),
+      animate('500ms ease-in-out', style({
+        opacity: 1,
+        transform: 'rotate(0)'
+      }))
+    ]);
+    const player = directiveAnimation.create(this.element.nativeElement);
+    player.play();
+  }
 
 }
