4a9860aca16cf1522fc7172cbf0df4ef64d42f9f
Andy Alcantara <andyalcantara745@yahoo.com>
2019-08-06 22:05:38 -0400

    Add animation logic for light speed in

--- src/app/speed-directives/light-speed-in.directive.ts
+++ src/app/speed-directives/light-speed-in.directive.ts
@@ -1,10 +1,42 @@
-import { Directive } from '@angular/core';
+import { Directive, Input, ElementRef, OnInit } from '@angular/core';
+import { AnimationBuilder, animate, style, keyframes } from '@angular/animations';
 
 @Directive({
   selector: '[animLightSpeedIn]'
 })
-export class LightSpeedInDirective {
+export class LightSpeedInDirective implements OnInit {
 
-  constructor() { }
+  constructor(
+    private animationBuilder: AnimationBuilder,
+    private element: ElementRef
+  ) { }
+
+  ngOnInit(): void {
+    const directiveAnimation = this.animationBuilder.build([
+      animate(400, keyframes([
+        style({
+          opacity: 0,
+          transform: 'translateX(400px) skewX(-22deg)'
+        }),
+        style({
+          opacity: 1,
+          transform: 'translateX(0)'
+        })
+      ])),
+      animate(400, keyframes([
+        style({
+          transform: 'skewX(22deg)'
+        }),
+        style({
+          transform: 'skewX(-10deg)'
+        }),
+        style({
+          transform: 'skewX(0)'
+        })
+      ]))
+    ]);
+    const player = directiveAnimation.create(this.element.nativeElement);
+    player.play();
+  }
 
 }
