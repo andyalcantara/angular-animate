ea6a52f8caf10c58b8b070b513b714211a3294b5
Andy Alcantara <andyalcantara745@yahoo.com>
2019-08-06 22:12:22 -0400

    Add animation logic to light speed out directive

--- src/app/speed-directives/light-speed-out.directive.ts
+++ src/app/speed-directives/light-speed-out.directive.ts
@@ -1,10 +1,31 @@
-import { Directive } from '@angular/core';
+import {Directive, ElementRef, OnInit} from '@angular/core';
+import {animate, AnimationBuilder, keyframes, style} from '@angular/animations';
 
 @Directive({
   selector: '[animLightSpeedOut]'
 })
-export class LightSpeedOutDirective {
+export class LightSpeedOutDirective implements OnInit {
 
-  constructor() { }
+  constructor(
+    private animationBuilder: AnimationBuilder,
+    private element: ElementRef
+  ) { }
+
+  ngOnInit(): void {
+    const directiveAnimation = this.animationBuilder.build([
+      animate(700, keyframes([
+        style({
+          opacity: 1,
+          transform: 'translateX(0)'
+        }),
+        style({
+          opacity: 0,
+          transform: 'translateX(400px) skewX(42deg)'
+        })
+      ]))
+    ]);
+    const player = directiveAnimation.create(this.element.nativeElement);
+    player.play();
+  }
 
 }
