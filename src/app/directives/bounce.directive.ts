32c1dbb6efbc75b776928666bed042e43309a5f5
Andy Alcantara <andyalcantara745@yahoo.com>
2019-08-06 21:41:08 -0400

    Add repeat logic to bounce

--- src/app/directives/bounce.directive.ts
+++ src/app/directives/bounce.directive.ts
@@ -43,10 +43,12 @@ export class BounceDirective implements OnInit {
       }, this.delay);
 
       player.onDone(() => {
+        let counter = 0;
         for (let i = 1; i < this.repeat; i++) {
+          counter = i;
           setTimeout(() => {
             player.play();
-          }, i * this.delay);
+          }, counter * this.delay);
         }
       });
   }
