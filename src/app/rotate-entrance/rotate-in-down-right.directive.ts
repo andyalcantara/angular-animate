dafe629250b721c95a9a0130e40508fce13b8ab4
Andy Alcantara <andyalcantara745@yahoo.com>
2019-08-07 13:54:06 -0400

    Add rotate in down right animation logic

--- src/app/rotate-entrance/rotate-in-down-right.directive.ts
+++ src/app/rotate-entrance/rotate-in-down-right.directive.ts
@@ -1,10 +1,30 @@
-import { Directive } from '@angular/core';
+import {Directive, ElementRef, OnInit} from '@angular/core';
+import {animate, AnimationBuilder, style} from '@angular/animations';
 
 @Directive({
   selector: '[animRotateInDownRight]'
 })
-export class RotateInDownRightDirective {
+export class RotateInDownRightDirective implements OnInit {
 
-  constructor() { }
+  constructor(
+    private animationBuilder: AnimationBuilder,
+    private element: ElementRef
+  ) { }
+
+  ngOnInit(): void {
+    const directiveAnimation = this.animationBuilder.build([
+      animate('0ms ease-in-out', style({
+        opacity: 0,
+        transformOrigin: 'bottom right',
+        transform: 'rotate(45deg)'
+      })),
+      animate('500ms ease-in-out', style({
+        opacity: 1,
+        transform: 'rotate(0)'
+      }))
+    ]);
+    const player = directiveAnimation.create(this.element.nativeElement);
+    player.play();
+  }
 
 }
