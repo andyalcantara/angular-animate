fd5df4715d1874c3eee472d64be0a7c6ffaeb7ee
Andy Alcantara <andyalcantara745@yahoo.com>
2019-08-07 23:01:04 -0400

    Add rotate out down left animation logic

--- src/app/rotate-out/rotate-out-down-left.directive.ts
+++ src/app/rotate-out/rotate-out-down-left.directive.ts
@@ -1,10 +1,30 @@
-import { Directive } from '@angular/core';
+import {Directive, ElementRef, OnInit} from '@angular/core';
+import {animate, AnimationBuilder, style} from '@angular/animations';
 
 @Directive({
   selector: '[animRotateOutDownLeft]'
 })
-export class RotateOutDownLeftDirective {
+export class RotateOutDownLeftDirective implements OnInit {
 
-  constructor() { }
+  constructor(
+    private animationBuilder: AnimationBuilder,
+    private element: ElementRef
+  ) { }
+
+  ngOnInit(): void {
+    const directiveAnimation = this.animationBuilder.build([
+      animate('0ms ease-in-out', style({
+        opacity: 1,
+        transform: 'rotate(0)'
+      })),
+      animate('500ms ease-in-out', style({
+        opacity: 0,
+        transformOrigin: 'bottom left',
+        transform: 'rotate(45deg)'
+      }))
+    ]);
+    const player = directiveAnimation.create(this.element.nativeElement);
+    player.play();
+  }
 
 }
