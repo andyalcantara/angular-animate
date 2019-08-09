89242f69b0e797728a65853dcf1bcefb70557bb5
Andy Alcantara <andyalcantara745@yahoo.com>
2019-08-08 22:22:08 -0400

    Add zoom out animation logic

--- src/app/zoom-out-directives/zoom-out.directive.ts
+++ src/app/zoom-out-directives/zoom-out.directive.ts
@@ -1,10 +1,29 @@
-import { Directive } from '@angular/core';
+import {Directive, ElementRef, OnInit} from '@angular/core';
+import {animate, AnimationBuilder, style} from '@angular/animations';
 
 @Directive({
   selector: '[animZoomOut]'
 })
-export class ZoomOutDirective {
+export class ZoomOutDirective implements OnInit {
 
-  constructor() { }
+  constructor(
+    private animationBuilder: AnimationBuilder,
+    private element: ElementRef
+  ) { }
+
+  ngOnInit(): void {
+    const directiveAnimation = this.animationBuilder.build([
+      animate('0ms', style({
+        opacity: 1,
+        transform: 'scale(1.0)'
+      })),
+      animate('600ms ease-out', style({
+        opacity: 0,
+        transform: 'scale(0)'
+      }))
+    ]);
+    const player = directiveAnimation.create(this.element.nativeElement);
+    player.play();
+  }
 
 }
