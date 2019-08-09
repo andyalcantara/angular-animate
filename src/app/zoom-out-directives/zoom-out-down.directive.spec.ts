e6a2e08efcd98f00ada81d3ed58c83a3fa9b2e01
Andy Alcantara <andyalcantara745@yahoo.com>
2019-08-08 22:31:40 -0400

    Add zoom out down animation logic

---
+++ src/app/zoom-out-directives/zoom-out-down.directive.spec.ts
@@ -0,0 +1,8 @@
+import { ZoomOutDownDirective } from './zoom-out-down.directive';
+
+describe('ZoomOutDownDirective', () => {
+  it('should create an instance', () => {
+    const directive = new ZoomOutDownDirective();
+    expect(directive).toBeTruthy();
+  });
+});
