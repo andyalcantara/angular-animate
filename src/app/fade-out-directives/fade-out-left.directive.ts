c4b781746d38507ae61f352f69a23f09b693af79
Andy Alcantara <andyalcantara745@yahoo.com>
2019-08-13 23:04:27 -0400

    Add delay and repeat to fade out directives folder

--- src/app/fade-out-directives/fade-out-left.directive.ts
+++ src/app/fade-out-directives/fade-out-left.directive.ts
@@ -1,10 +1,13 @@
-import {Directive, ElementRef, OnInit} from '@angular/core';
+import {Directive, ElementRef, Input, OnInit} from '@angular/core';
 import {animate, AnimationBuilder, style} from '@angular/animations';
 
 @Directive({
   selector: '[animFadeOutLeft]'
 })
 export class FadeOutLeftDirective implements OnInit {
+
+  @Input() delay = 0;
+  @Input() repeat = 0;
 
   constructor(
     private animationBuilder: AnimationBuilder,
@@ -22,7 +25,18 @@ export class FadeOutLeftDirective implements OnInit {
       })),
     ]);
     const player = directiveAnimation.create(this.element.nativeElement);
-    player.play();
+
+    setTimeout(() => {
+      player.play();
+    });
+
+    player.onDone(() => {
+      for (let i = 1; i < this.repeat; i++) {
+        setTimeout(() => {
+          player.play();
+        }, i * this.delay);
+      }
+    });
   }
 
 }
--- src/app/fade-out-directives/fade-out-right-big.directive.ts
+++ src/app/fade-out-directives/fade-out-right-big.directive.ts
@@ -1,10 +1,13 @@
-import {Directive, ElementRef, OnInit} from '@angular/core';
+import {Directive, ElementRef, Input, OnInit} from '@angular/core';
 import {animate, AnimationBuilder, style} from '@angular/animations';
 
 @Directive({
   selector: '[animFadeOutRightBig]'
 })
 export class FadeOutRightBigDirective implements OnInit {
+
+  @Input() delay = 0;
+  @Input() repeat = 0;
 
   constructor(
     private animationBuilder: AnimationBuilder,
@@ -22,7 +25,18 @@ export class FadeOutRightBigDirective implements OnInit {
       })),
     ]);
     const player = directiveAnimation.create(this.element.nativeElement);
-    player.play();
+
+    setTimeout(() => {
+      player.play();
+    });
+
+    player.onDone(() => {
+      for (let i = 1; i < this.repeat; i++) {
+        setTimeout(() => {
+          player.play();
+        }, i * this.delay);
+      }
+    });
   }
 
 }
