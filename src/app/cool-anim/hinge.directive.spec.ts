d58a1ae6243841b4ff57ce175dd75f7ac5af5df5
Andy Alcantara <andyalcantara745@yahoo.com>
2019-08-14 12:06:31 -0400

    Add test to hinge directive

--- src/app/cool-anim/hinge.directive.spec.ts
+++ src/app/cool-anim/hinge.directive.spec.ts
@@ -1,8 +1,18 @@
 import { HingeDirective } from './hinge.directive';
+import {TestBed} from '@angular/core/testing';
+import {AppComponent} from '../app.component';
 
-describe('HingeDirective', () => {
-  it('should create an instance', () => {
-    const directive = new HingeDirective();
-    expect(directive).toBeTruthy();
+fdescribe('HingeDirective', () => {
+  beforeEach(() => {
+    const fixture = TestBed.configureTestingModule({
+      declarations: [AppComponent, HingeDirective]
+    })
+      .createComponent(HingeDirective);
+    fixture.detectChanges();
+
+    it('delay should be zero if not input given', () => {
+      const delay: number = fixture.componentInstance.delay;
+      expect(delay).toBe(0);
+    });
   });
 });
