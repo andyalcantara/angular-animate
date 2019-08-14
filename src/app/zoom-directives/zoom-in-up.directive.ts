1f57290cf760bf173cf7b98e7186d935593bee05
Andy Alcantara <andyalcantara745@yahoo.com>
2019-08-13 23:40:07 -0400

    Add delay and repeat to zoom directives

--- src/app/zoom-directives/zoom-in-up.directive.ts
+++ src/app/zoom-directives/zoom-in-up.directive.ts
@@ -1,10 +1,13 @@
-import {Directive, ElementRef, OnInit} from '@angular/core';
+import {Directive, ElementRef, Input, OnInit} from '@angular/core';
 import {animate, AnimationBuilder, keyframes, style} from '@angular/animations';
 
 @Directive({
   selector: '[animZoomInUp]'
 })
 export class ZoomInUpDirective implements OnInit {
+
+  @Input() delay = 0;
+  @Input() repeat = 0;
 
   constructor(
     private animationBuilder: AnimationBuilder,
@@ -28,7 +31,18 @@ export class ZoomInUpDirective implements OnInit {
       ]))
     ]);
     const player = directiveAnimation.create(this.element.nativeElement);
-    player.play();
+
+    setTimeout(() => {
+      player.play();
+    }, this.delay);
+
+    player.onDone(() => {
+      for (let i = 1; i < this.repeat; i++) {
+        setTimeout(() => {
+          player.play();
+        }, i * this.delay);
+      }
+    });
   }
 
 }
--- src/app/zoom-directives/zoom-in.directive.ts
+++ src/app/zoom-directives/zoom-in.directive.ts
@@ -1,10 +1,13 @@
-import {Directive, ElementRef, OnInit} from '@angular/core';
+import {Directive, ElementRef, Input, OnInit} from '@angular/core';
 import {animate, AnimationBuilder, style, keyframes} from '@angular/animations';
 
 @Directive({
   selector: '[animZoomIn]'
 })
 export class ZoomInDirective implements OnInit {
+
+  @Input() delay = 0;
+  @Input() repeat = 0;
 
   constructor(
     private animationBuilder: AnimationBuilder,
@@ -21,7 +24,18 @@ export class ZoomInDirective implements OnInit {
       }))
     ]);
     const player = directiveAnimation.create(this.element.nativeElement);
-    player.play();
+
+    setTimeout(() => {
+      player.play();
+    }, this.delay);
+
+    player.onDone(() => {
+      for (let i = 1; i < this.repeat; i++) {
+        setTimeout(() => {
+          player.play();
+        }, i * this.delay);
+      }
+    });
   }
 
 }
