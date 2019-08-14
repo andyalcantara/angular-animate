6c1c25cc711ebf9d3982cde7a2f39b9aa0ce0800
Andy Alcantara <andyalcantara745@yahoo.com>
2019-08-13 22:48:14 -0400

    Add delay and repeat to cool-anim folder directives

--- src/app/cool-anim/roll-out.directive.ts
+++ src/app/cool-anim/roll-out.directive.ts
@@ -1,10 +1,13 @@
-import {Directive, ElementRef, OnInit} from '@angular/core';
+import {Directive, ElementRef, Input, OnInit} from '@angular/core';
 import {animate, AnimationBuilder, style} from '@angular/animations';
 
 @Directive({
   selector: '[animRollOut]'
 })
 export class RollOutDirective implements OnInit {
+
+  @Input() delay = 0;
+  @Input() repeat = 0;
 
   constructor(
     private animationBuilder: AnimationBuilder,
@@ -22,7 +25,17 @@ export class RollOutDirective implements OnInit {
       }))
     ]);
     const player = directiveAnimation.create(this.element.nativeElement);
-    player.play();
+    setTimeout(() => {
+      player.play();
+    }, this.delay);
+
+    player.onDone(() => {
+      for (let i = 1; i < this.repeat; i++) {
+        setTimeout(() => {
+          player.play();
+        }, i * this.delay);
+      }
+    });
   }
 
 }
