0a4e0ad12f1f249b3fb14724cbc31390f13bce7c
Andy Alcantara <andyalcantara745@yahoo.com>
2019-08-14 15:27:49 -0400

    Uninstall dependecies that won't be used

--- src/app/in-from-top.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { InFromTopDirective } from './in-from-top.directive';
-
-describe('InFromTopDirective', () => {
-  it('should create an instance', () => {
-    const directive = new InFromTopDirective();
-    expect(directive).toBeTruthy();
-  });
-});
