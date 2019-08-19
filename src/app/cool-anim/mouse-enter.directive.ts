49a5f84bbeb024aecb5ae03f7c74ff25cd537c42
Andy Alcantara <andyalcantara745@yahoo.com>
2019-08-18 23:01:16 -0400

    Add basic structure to mouse enter directive

--- src/app/cool-anim/mouse-enter.directive.ts
+++ src/app/cool-anim/mouse-enter.directive.ts
@@ -13,5 +13,30 @@ export class MouseEnterDirective {
 
   @HostListener('mouseenter', ['$event']) onMouseEnter(event: Event) {
     console.log(event);
+    const directiveAnimation = this.animationBuilder.build([
+      animate('0ms ease-in', style({
+        backgroundPosition: 'right'
+      })),
+      animate('500ms ease-in', style({
+        width: '100%',
+        height: '100%',
+        borderBottomRightRadius: '0'
+      }))
+    ]);
+    const player = directiveAnimation.create(this.element.nativeElement);
+    player.play();
+  }
+
+  @HostListener('mouseout', ['$event']) onMouseOut(event: Event) {
+    console.log(event);
+    const directiveAnimation = this.animationBuilder.build([
+      animate('500ms ease-in', style({
+        width: '40%',
+        height: '100px',
+        borderBottomRightRadius: '0'
+      }))
+    ]);
+    const player = directiveAnimation.create(this.element.nativeElement);
+    player.play();
   }
 }
